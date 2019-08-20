5e74f3426bcf856386d98e9451a7aa8fe692ba91
Wilson Styres <wstyres@gmail.com>
2019-08-20 16:46:31 -0400

    New SwiftUI and Catalyst based project

--- Discression/AppDelegate.swift
+++ Discression/AppDelegate.swift
@@ -2,23 +2,34 @@
 //  AppDelegate.swift
 //  Discression
 //
-//  Created by Wilson Styres on 10/8/17.
-//  Copyright © 2017 Wilson Styres. All rights reserved.
+//  Created by Wilson Styres on 8/20/19.
+//  Copyright © 2019 Wilson Styres. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
 
---
+++ Discression/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Discression
+//
+//  Created by Wilson Styres on 8/20/19.
+//  Copyright © 2019 Wilson Styres. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
